9f2b70a7374f1744c11e137a6a6503a5225af130
santosh-chaugule <santoshchaugule1697@gmail.com>
2022-12-07 11:30:45 +0530

    JS

---
+++ script.js
@@ -0,0 +1,13 @@
+var student1_name = "Santosh";
+var student1_surname = "Chaugule";
+
+
+var student1 = {
+    // 1 Property:Value
+    //2...
+
+    name: "Santosh",
+    surname: "Chaugule"
+}; //JS object 
+console.log("Hello " + student1_name)
+//this + is the concatination operator
